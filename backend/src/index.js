0ccca0421fac5bef7965236b9a72df129c965507
b2pkick <rahulgupta79777@gmail.com>
2025-09-18 13:02:00 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import {connectDB} from "./lib/db.js"
+
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config()
+const app = express()
+
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+    connectDB()
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-7xl text-yellow-300'>hello world my name is rahul gupta and i am doing btech cse from chitkara university</h1>
+    </>
+  )
+}
+
+export default App
