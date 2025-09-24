9d4df749d9bc3da84a1f1bd95b4fdc212f580fdd
b2pkick <rahulgupta79777@gmail.com>
2025-09-24 23:26:27 +0530

    made message controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,3 +109,12 @@ export const updateProfile = async(req,res)=>{
         res.status(500).json({message:"Internal Server Error"})
     }
 }
+
+export const checkAuth = (req,res) =>{
+    try{
+        req.status(200).json(req.user)
+    }catch(error){
+        console.log("error in checkauth controller".error.message)
+        res.status(500).json({message:"internal server error"})
+    }
+}
