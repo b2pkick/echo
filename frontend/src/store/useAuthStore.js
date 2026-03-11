7a091b8df6e6e9bca8ac3c3e33cdf8736cdeeedf
b2pkick <rahulgupta79777@gmail.com>
2026-03-11 18:37:00 +0530

    frontend railway change

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -133,7 +133,7 @@ export const useAuthStore = create((set,get)=>({
     connectSocket:()=>{
         const {authUser}=get()
         if(!authUser||get().socket?.connected) return
-        const socket =io("http://localhost:5001",{
+        const socket =io("echo-production-cc43.up.railway.app",{
             query:{
                 userId:authUser._id
             }
