314e5c3560f3ed61fda1db2d9117d34d24f5d504
b2pkick <rahulgupta79777@gmail.com>
2026-03-11 23:18:18 +0530

    fix deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,10 +16,10 @@ const port = process.env.PORT||5001
 
 // app.use(express.json())
 app.use(cookieParser())
-// app.use(cors({
-//     origin:["http://localhost:5173","https://echo-eight-xi.vercel.app","*"],
-//     credentials:true
-// }))
+app.use(cors({
+    origin:["http://localhost:5173","https://echo-eight-xi.vercel.app","*"],
+    credentials:true
+}))
 app.use(express.json({limit:"10mb"}))
 app.use(express.urlencoded({limit:"10mb",extended:true}))
 
