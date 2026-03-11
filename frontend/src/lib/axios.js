a33ea55b84c2677e2ef7b4b40587c00740614718
b2pkick <rahulgupta79777@gmail.com>
2026-03-11 18:28:06 +0530

    render frontend change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:"echo-production-cc43.up.railway.app/api",
     withCredentials:true
 })
